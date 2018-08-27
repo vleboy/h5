e9895397bb68cecaa8be4336b5747cb121b3ede1
xiaofeng.tu <441664184@qq.com>
2018-08-27 15:02:39 +0800

    [dev] 合并

--- caishen/src/game/ui/GameScene.ts
+++ caishen/src/game/ui/GameScene.ts
@@ -169,7 +169,7 @@ module game {
 		/**控制游戏状态 */
 		private setState(n: GameState){
 			this.state = n;
-			// this.bottomBar.setState(n);
+			this.bottomBar.setState(n);
 		}
 		/**spin的逻辑 */
 		private spin(){
