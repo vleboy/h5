54d59cec42175d629254e433064a2e82607f92b8
tm18481599829 <18481599829@qq.com>
2018-09-04 15:58:06 +0800

    [dev] bottom payout添加动画

--- caishen/src/game/ui/BottomBar.ts
+++ caishen/src/game/ui/BottomBar.ts
@@ -1,5 +1,6 @@
 module game {
 	export class BottomBar extends BaseUI {
+		private groupBtm: eui.Group;
 		/**说明按钮*/
 		private helpBtn: eui.Button;
 		/**游戏开始转动按钮*/
@@ -38,6 +39,8 @@ module game {
 		private BtnMax: eui.Button;
 		/**单注金额*/
 		private betTxt: eui.Label;
+		/**中间闪烁图片*/
+		private winLight:eui.Image;
 		//--------------变量-------------
 		/**单注数字数组*/
 		private theBetArr: number[];
@@ -201,15 +204,49 @@ module game {
 			this.groupAuto.visible = this.isAuto;
 		}
 		/**获得派彩的动画*/
-		private payout(mon: number) {
+		private payout(mon: number): void {
 			this.winNum = 0;
 			egret.Tween.get(this, { onChange: () => { this.winTxt.text = this.winNum.toFixed(2); }, onChangeObj: this })
 				.to({ winNum: mon }, 800)
-				.call(() => {egret.Tween.removeTweens(this);});
+				.call(() => { egret.Tween.removeTweens(this); });
 			egret.Tween.get(this.winTxt)
 				.to({ scaleX: 1.5, scaleY: 1.5 }, 400)
 				.to({ scaleX: 1, scaleY: 1 }, 400)
-				.call(() => {egret.Tween.removeTweens(this.winTxt);});
+				.call(() => { egret.Tween.removeTweens(this.winTxt); });
+			let lightMove = () => {
+				//底部粒子效果
+				let texture = RES.getRes("light_lizi01_png");
+				let cfg = RES.getRes("particle_btmLight_json");
+				let theParticle: particle.GravityParticleSystem = new particle.GravityParticleSystem(texture, cfg);
+				theParticle.emitterX = 0;
+				theParticle.emitterY = 210;
+				theParticle.start();
+				this.groupBtm.addChild(theParticle);
+				egret.Tween.get(theParticle)
+					.to({emitterX:750},310)
+					.to({emitterX:800,emitterY:84},20)
+					.to({emitterX:1105},140)
+					.to({emitterX:1155,emitterY:210},20)
+					.to({emitterX:1900},310)
+					.call(()=>{
+						egret.Tween.removeTweens(theParticle);
+						theParticle.stop();
+						theParticle.visible = false;
+					});
+			}
+			lightMove();
+
+			this.winLight.visible = true;
+			egret.Tween.get(this.winLight)
+				.to({aplha:0.7},200)
+				.to({aplha:1},200)
+				.to({aplha:0.7},200)
+				.to({aplha:1},200)
+				.call(()=>{
+					egret.Tween.removeTweens(this.winLight);
+					this.winLight.visible = false;
+				})
+			
 		}
 		/**图片旋转
 		 * @param isStop 是不是停止动画
