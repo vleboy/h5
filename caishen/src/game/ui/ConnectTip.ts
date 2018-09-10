6437a82c8c436638de7c694cd3d7bc30bac75d6e
tm18481599829 <18481599829@qq.com>
2018-09-10 14:38:04 +0800

    [dev] 显示加载

--- caishen/src/game/ui/ConnectTip.ts
+++ caishen/src/game/ui/ConnectTip.ts
@@ -1,12 +1,13 @@
 module game {
-	export class ConnectTip extends BaseUI{
+	export class ConnectTip extends BaseUI {
 		public constructor() {
 			super();
 			this.skinName = GlobalConfig.skinPath + "connectTipSkin.exml";
 		}
-		public init(){
-			egret.Tween.get(this["circle"], {loop:true})
-				.to({rotation:360}, 2000, egret.Ease.circInOut);
+		public init() {}
+		public show(isShow: boolean): void {
+			this.visible = isShow;
+			isShow ? egret.Tween.get(this["circle"], { loop: true }).to({ rotation: 360 }, 1000, egret.Ease.circInOut) : egret.Tween.removeTweens(this["circle"]);
 		}
 	}
 }
