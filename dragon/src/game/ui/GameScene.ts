d135d508476b0d5579b4872f6406f4e65c860f69
xiaofeng.tu <441664184@qq.com>
2018-09-20 19:47:34 +0800

    替换免费游戏中再获得免费机会的粒子拖尾效果

--- dragon/src/game/ui/GameScene.ts
+++ dragon/src/game/ui/GameScene.ts
@@ -935,22 +935,43 @@ module game {
 			this.freeChooseCountBoom.x = 960;
 			this.freeChooseCountBoom.y = 540;
 
+			
+
 			let isShow: boolean = this.featureChanceCount > 0;
 			if (isAn) {
-				isShow && egret.Tween.get(this.freeChooseCountBoom)
-					.call(() => this.freeChooseCountBoom.visible = true)
-					.to({ scaleX: 0.3, scaleY: 0.3, x: 1532, y: 153 }, 1000)
-					.to({ scaleX: 1.2, scaleY: 1.2 }, 10)
-					.call(() => {
-						this.freeChooseCountBoom.play();
-						this.freeChooseCountTxt.text = "x" + this.featureChanceCount;
-						this.freeChooseCountBg.visible = isShow;
-						this.freeChooseCountTxt.visible = isShow;
-						setTimeout(() => {
-							this.freeChooseCountBoom.stop();
-							this.freeChooseCountBoom.visible = false;
-						}, 1000)
-					})
+				if(isShow){
+					let p = new particle.GravityParticleSystem(RES.getRes("freeChanceParticle_png"), RES.getRes("freeChanceParticle_json"));
+					this.freeChooseCountBoom.parent.addChild(p);
+					p.blendMode = egret.BlendMode.ADD;
+					p.emitterX = this.freeChooseCountBoom.x;
+					p.emitterY = this.freeChooseCountBoom.y;
+					p.start();
+
+					egret.Tween.get(this.freeChooseCountBoom, {onChange:()=>{
+						p.emitterX = this.freeChooseCountBoom.x;
+						p.emitterY = this.freeChooseCountBoom.y;
+					}, onChangeObj:this})
+						.call(() => this.freeChooseCountBoom.visible = true)
+						.to({ scaleX: 0.3, scaleY: 0.3, x: 1532, y: 153 }, 1000)
+						.call(()=>{
+							p.stop();
+						})
+						.to({ scaleX: 1.2, scaleY: 1.2 }, 10)
+						.call(() => {
+							this.freeChooseCountBoom.play();
+							this.freeChooseCountTxt.text = "x" + this.featureChanceCount;
+							this.freeChooseCountBg.visible = isShow;
+							this.freeChooseCountTxt.visible = isShow;
+							setTimeout(() => {
+								if(p && p.parent){
+									p.parent.removeChild(p);
+								}
+								this.freeChooseCountBoom.stop();
+								this.freeChooseCountBoom.visible = false;
+							}, 1000)
+						})
+				}
+				
 			} else {
 				this.freeChooseCountBg.visible = isShow;
 				this.freeChooseCountTxt.visible = isShow;
