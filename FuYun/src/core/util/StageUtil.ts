c699211a784f47205132489451e7a9783bf6a5ac
tm18481599829 <18481599829@qq.com>
2018-09-25 10:42:03 +0800

    [dev] FuYun

---
+++ FuYun/src/core/util/StageUtil.ts
@@ -0,0 +1,8 @@
+module game {
+	export class StageUtil {
+		public constructor() {
+		}
+
+		public static stage: egret.Stage;
+	}
+}
---
+++ FuYun/src/game/ui/Tips.ts
@@ -0,0 +1,7 @@
+module game {
+	export class Tips extends eui.Component{
+		public constructor() {
+			super();
+		}
+	}
+}
